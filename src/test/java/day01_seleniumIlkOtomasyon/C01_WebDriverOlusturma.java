897db3fcece75f064f7bc5fb621505c80b4aa328
Aybar <you@example.com>
2023-11-13 14:48:29 +0300

    first commit

---
+++ src/test/java/day01_seleniumIlkOtomasyon/C01_WebDriverOlusturma.java
@@ -0,0 +1,26 @@
+package day01_seleniumIlkOtomasyon;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_WebDriverOlusturma {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("Webdriver.chrome.driver","src/resources/chromedriver.exe");
+        // Bu satır oluşturucağımız webdriver'ın özelliklerin set ediyor
+        // webdriver otomasyon sırasında bizim elimiz, gözümüzi herşeyimizdir
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.testotomasyonu.com/");
+
+        Thread.sleep(5000);
+        driver.manage().window().maximize();
+
+        Thread.sleep(5000);
+
+        driver.close();
+
+
+    }
+}
